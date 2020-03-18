0b829f862888af0d28f8b21dd521b240f942db52
Bhuvana Jothivel <58466121+BhuvanaJothivel@users.noreply.github.com>
2020-03-18 10:22:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,30 +21,47 @@ const PLAYERS = [
     "Slingo"
 ];
 
+var i = 1;
 // initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = '';
+const initPlayers = (PLAYERS) => {
+    let playerDetails = '';
 
     // Instead of forloop use Map method
     // Code here
+    playerDetails = PLAYERS.map((player,i) => {
+        console.log(i);
+        return {
+            name: player,
+            image: "images/super-" + (i) + ".png",
+            strength: getRandomStrength(),
+            type: (i%2 == 0)?"hero":"villain"
+        }
+    });
 
-    return detailedPlayers;
+    return playerDetails;
 }
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
+    return Math.floor(Math.random() * 100);
 }
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let temp = '';
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    
 
-    return fragment;
+    temp = players.filter(player => player.type == type).map(player =>
+    `<div class="player">
+    <img src="${player.image}" alt=" ">
+    <div class="name">${player.name}</div>
+    <div class="strength">${player.strength}</div>
+    </div>`).join();
+    return temp;
 }
 
 // Display players in HTML
